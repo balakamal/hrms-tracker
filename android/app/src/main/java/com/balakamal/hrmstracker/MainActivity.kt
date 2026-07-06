41f8285d4136e0d00dc1a4b727ac9f460f4c2137
kamal.thiruveedhula <kamal.thiruveedhula@pal.tech>
2026-07-06 22:12:05 +0530

    Add native Android app WebView wrapper and GitHub Actions compilation workflow

---
+++ android/app/src/main/java/com/balakamal/hrmstracker/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.balakamal.hrmstracker
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val webView = WebView(this)
+        setContentView(webView)
+        
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                return false
+            }
+        }
+        
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        webSettings.cacheMode = WebSettings.LOAD_DEFAULT
+        
+        webView.loadUrl("https://balakamal.github.io/hrms-tracker/hrms-pwa/")
+    }
+}
